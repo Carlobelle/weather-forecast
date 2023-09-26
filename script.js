21d544a864e28ab05a2c730402b60da594f15a1d
Carlovsky Bellefleur <carlovskybellefleur@Carlovskys-MacBook-Pro.local>
2023-09-26 00:02:23 -0300

    worked on js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var api= "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=7314d9fbeae6a7a404de26fe55694226"
-fetch(api)
-.then(function(Response){
-    console.log(Response)
-    return Response.json()
-})
-.then(function(data){
-console.log (data)
-})
+var api = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=7314d9fbeae6a7a404de26fe55694226"
+var searchBtn = document.querySelector('.searchBtn')
+var Apikey='7314d9fbeae6a7a404de26fe55694226'
+searchBtn.addEventListener('click', searchCity)
+function searchCity() {
+    var search = document.querySelector('.search')
+    searchApi(search.value)
+}
+function searchApi(cityname){
+    var Api=`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${APIkey}`
+}
